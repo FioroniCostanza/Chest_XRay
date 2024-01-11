f6aa1585eafc7c9bfed22ee4353c82209b17e221
Costanza Fioroni <117634064+FioroniCostanza@users.noreply.github.com>
2024-01-11 20:26:11 +0300

    minor fix

--- rename_images.py
+++ rename_images.py
@@ -1,7 +1,7 @@
 import os
 
 def rename_images(folder_path):
-    # first phase: use a temporary
+    # First phase: use a temporary
     temp_prefix = "temporary_"
     files = [f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f)) and f.lower().endswith(('.png','.jpg', '.jpeg')) and f != '.DS_Store']
     for file in files:
@@ -10,7 +10,7 @@ def rename_images(folder_path):
         print(file)
         os.rename(old_file_path, temp_file_path)
 
-    # Second phase: Rimuove the prefix and assign the progressive numbers
+    # Second phase: Remove the prefix and assign the progressive numbers
     temp_files = [f for f in os.listdir(folder_path) if f.startswith(temp_prefix)]
     temp_files.sort()
     for i, temp_file in enumerate(temp_files, start=0):
@@ -22,7 +22,7 @@ def rename_images(folder_path):
 
 # put the root of the dataset
 base_path = 'C:/Users/antos/Desktop/dataset_pulito/chest_xray'
-#put the names of the class which were merged 
+# put the names of the class which were merged 
 folders = ['Normal', 'Tuberculosis', 'Pneumonia']
 
 for folder in folders:
